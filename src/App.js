721fa0487c79c0ec738f8a9b06642bc21ab33ecf
MweleCarol <mwelecarolyne65@gmail.com>
2026-04-02 16:29:52 +0300

    Implementend the fist part of the navbar

--- src/App.js
+++ src/App.js
@@ -5,9 +5,11 @@ import {Ecommerce, Orders, Calendar, Employees, Stacked, Pyramid, Customers, Kan
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { FiSettings } from 'react-icons/fi';
 import {TooltipComponent} from '@syncfusion/ej2-react-popups';
+import { useStateContext } from './contexts/ContextProvider';
 
 const App = () => {
-  const activeMenu = true;
+
+  const {activeMenu} = useStateContext();
 
   return (
    <div>
